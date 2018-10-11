eeb3b8fc4d0953c0b9a20a587aba4e4a36c50fb8
consgo <sgontzos@csd.uoc.gr>
2018-10-11 13:12:10 +0300

    Added time efficiency class.

--- src/main/java/gr/forth/ics/isl/demo/evaluation/HotelDemoEfficiencyTestSuit.java
+++ src/main/java/gr/forth/ics/isl/demo/evaluation/HotelDemoEfficiencyTestSuit.java
@@ -12,157 +12,156 @@ package gr.forth.ics.isl.demo.evaluation;
 import com.crtomirmajer.wmd4j.WordMovers;
 import edu.mit.jwi.Dictionary;
 import edu.mit.jwi.IDictionary;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import gr.forth.ics.isl.demo.auxiliaryClasses.Timer;
-import gr.forth.ics.isl.demo.models.BaselineModel;
+import gr.forth.ics.isl.demo.evaluation.models.EvaluationPair;
+import gr.forth.ics.isl.demo.evaluation.models.ModelHyperparameters;
 import gr.forth.ics.isl.demo.models.Model;
 import gr.forth.ics.isl.demo.models.Word2vecModel;
+import gr.forth.ics.isl.demo.models.Word2vecModel_III;
 import gr.forth.ics.isl.demo.models.WordnetModel;
 import gr.forth.ics.isl.demo.models.WordnetWord2vecModel;
+import gr.forth.ics.isl.demo.models.WordnetWord2vecModel_III;
+import gr.forth.ics.isl.demoCombined.main.combinedDemoMain;
+import static gr.forth.ics.isl.main.demo_main.getCommentsFromBooking;
 import gr.forth.ics.isl.nlp.models.Comment;
 import gr.forth.ics.isl.sailInfoBase.QAInfoBase;
 import gr.forth.ics.isl.sailInfoBase.models.Subject;
 import gr.forth.ics.isl.utilities.StringUtils;
+import gr.forth.ics.isl.utilities.Utils;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import mitos.stemmer.trie.Trie;
+import java.util.Properties;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.repository.RepositoryException;
-import static gr.forth.ics.isl.main.demo_main.getCommentsFromBooking;
 
 /**
  *
  * @author Sgo
  */
-
-
 public class HotelDemoEfficiencyTestSuit {
 
-    //Number of top comments to retrieve
-    static int topK = 10;
+    public static void main(String[] args) throws RepositoryException, MalformedQueryException, QueryEvaluationException, IOException, FileNotFoundException, ClassNotFoundException {
 
-    //The paths for the stopWords Files.
-    public static String filePath_en = "src/main/resources/stoplists/stopwordsEn.txt";
-    public static String filePath_gr = "src/main/resources/stoplists/stopwordsGr.txt";
+        StringUtils.generateStopLists(StringUtils.filePath_en, StringUtils.filePath_gr);
+        QAInfoBase KB = new QAInfoBase();
 
-    //A hashMap that contains two Trie, one for the English stopList and one for the Greek
-    public static HashMap<String, Trie> stopLists = new HashMap<>();
+        HashSet<Subject> reviews = KB.getAllSubjectsOfType("hip", "review");
+        ArrayList<Comment> comments = getCommentsFromBooking(reviews);
+        System.out.println(comments.size());
 
-    public static Word2Vec w2_vector;
-    public static WordMovers word_movers;
-    public static IDictionary wordnet_dict;
-    public static ArrayList<Comment> all_comments;
-
-    public static void main(String[] args) throws RepositoryException, MalformedQueryException, QueryEvaluationException, IOException {
-
-        // Create the list of stopWords to use
-        StringUtils.generateStopLists(filePath_en, filePath_gr);
-
-        //Measure Init time
-        measureInitTime();
-
-        //Get the user's question
-        String question = "Has anyone reported a problem about noise?";
-
-        ArrayList<Model> models = getModelsToTest();
-
-        // Measure execution time for all models
-        measureExecutionTime(models, question);
-
-    }
-
-    public static ArrayList<Model> getModelsToTest() {
-        //Define models to test for Execution time
-        ArrayList<String> wordnetResources = new ArrayList<>();
-        wordnetResources.add("synonyms");
-        wordnetResources.add("antonyms");
-        wordnetResources.add("hypernyms");
-
-        HashMap<String, Float> model_weights = new HashMap<>();
-        model_weights.put("wordnet", 0.6f);
-        model_weights.put("word2vec", 0.4f);
-
-        BaselineModel baseline = new BaselineModel("Baseline model (Jaccard Similarity)", all_comments);
-        WordnetModel wordnet = new WordnetModel("Wordnet model", wordnet_dict, wordnetResources, all_comments);
-        Word2vecModel word2vec = new Word2vecModel("Word2vec model", word_movers, w2_vector, all_comments);
-        WordnetWord2vecModel combination = new WordnetWord2vecModel("Word2vec and Wordnet", wordnet_dict, wordnetResources, word_movers, w2_vector, model_weights, all_comments);
-
-        ArrayList<Model> models = new ArrayList<>();
-        models.add(baseline);
-        models.add(wordnet);
-        models.add(word2vec);
-        models.add(combination);
-
-        return models;
-    }
-
-    public static void measureInitTime() throws MalformedURLException, IOException, RepositoryException, MalformedQueryException, QueryEvaluationException {
-
-        //Intance of class timer, for time measurements
-        Timer timer = new Timer();
-
-        // Create the list of stopWords to use
-        StringUtils.generateStopLists(filePath_en, filePath_gr);
-
-        // Create Word2Vec model
         File gModel = new File("C:/Users/Sgo/Desktop/Developer/Vector Models/GoogleNews-vectors-negative300.bin.gz");
-
-        timer.start();
         Word2Vec vec = WordVectorSerializer.readWord2VecModel(gModel);
-        w2_vector = vec;
         WordMovers wm = WordMovers.Builder().wordVectors(vec).build();
-        word_movers = wm;
-        timer.end();
-        long word2vecTime = timer.getTotalTime();
-        System.out.println("Time to load word2vec: " + word2vecTime);
 
-        timer.start();
+        // Retrieve hyperparameters
+        ModelHyperparameters bestModel = (ModelHyperparameters) Utils.getSavedObject("AVEPbased_BestModel");
+        float word2vec_w = bestModel.getWord2vecWeight();
+        float wordNet_w = bestModel.getWordNetWeight();
+        // Choose weights to be used in model IV
+        HashMap<String, Float> model_weights = new HashMap<>();
+        model_weights.put("wordnet", wordNet_w);
+        model_weights.put("word2vec", word2vec_w);
+
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma");
+        props.put("tokenize.language", "en");
+        combinedDemoMain.pipeline = new StanfordCoreNLP(props);
+
+        ArrayList<String> contextWords = new ArrayList<>();
+        contextWords.add("problem");
+        contextWords.add("issue");
+        contextWords.add("report");
+        contextWords.add("hotel");
+        contextWords.add("complaint");
+        contextWords.add("anyone");
+        contextWords.add("complain");
+
         // Create WodNet Dictionary
         String wnhome = System.getenv("WNHOME");
         String path = wnhome + File.separator + "dict";
         URL url = new URL("file", null, path);
         // construct the dictionary object and open it
         IDictionary dict = new Dictionary(url);
-        wordnet_dict = dict;
         // open WordNet dictionary
         dict.open();
-        timer.end();
-        long wordNetTime = timer.getTotalTime();
-        System.out.println("Time to load WordNet: " + wordNetTime);
 
-        timer.start();
-        QAInfoBase KB = new QAInfoBase();
-        // Retrieve hotels
-        HashSet<Subject> reviews = KB.getAllSubjectsOfType("hip", "review");
+        // Choose wordnet sources to be used
+        ArrayList<String> wordnetResources = new ArrayList<>();
+        wordnetResources.add("synonyms");
+        wordnetResources.add("antonyms");
+        wordnetResources.add("hypernyms");
 
-        ArrayList<Comment> comments = getCommentsFromBooking(reviews);
-        all_comments = comments;
-        timer.end();
-        long resourcesTime = timer.getTotalTime();
-        System.out.println("Time to load resources: " + resourcesTime);
+        HashMap<String, HashMap<String, EvaluationPair>> gt = EvalCollectionManipulator.readEvaluationSet("FRUCE_v2.csv");
+
+        WordnetModel wordnet = new WordnetModel("Wordnet", dict, wordnetResources, comments);
+        Word2vecModel word2vec = new Word2vecModel("Word2vec model", wm, vec, comments);
+        Word2vecModel word2vec_cw = new Word2vecModel("Word2vec model Context Words", wm, vec, comments, contextWords);
+        WordnetWord2vecModel combination = new WordnetWord2vecModel("Word2vec and Wordnet", dict, wordnetResources, wm, vec, model_weights, comments);
+        WordnetWord2vecModel combination_cw = new WordnetWord2vecModel("Word2vec and Wordnet Context Words", dict, wordnetResources, wm, vec, model_weights, comments, contextWords);
+        Word2vecModel_III word2vec_III = new Word2vecModel_III("Word2vec model III", wm, vec, comments);
+        Word2vecModel_III word2vec_III_cw = new Word2vecModel_III("Word2vec model III Context Words", wm, vec, comments, contextWords);
+        WordnetWord2vecModel_III combination_III = new WordnetWord2vecModel_III("Word2vec and Wordnet III", dict, wordnetResources, wm, vec, model_weights, comments);
+        WordnetWord2vecModel_III combination_III_cw = new WordnetWord2vecModel_III("Word2vec and Wordnet III Context Words", dict, wordnetResources, wm, vec, model_weights, comments, contextWords);
+
+        ArrayList<Model> models = new ArrayList<>();
+        models.add(wordnet);
+        models.add(word2vec);
+        models.add(word2vec_cw);
+        models.add(combination);
+        models.add(combination_cw);
+        models.add(word2vec_III);
+        models.add(word2vec_III_cw);
+        models.add(combination_III);
+        models.add(combination_III_cw);
+
+        ArrayList<String> queryList = new ArrayList<>();
+        queryList.add("Has anyone reported a problem about noise?");
+        queryList.add("Is this hotel quiet?");
+        queryList.add("Has anyone reported a problem about cleanliness?");
+        queryList.add("Has anyone complained about the bed linen?");
+        queryList.add("Is the personnel polite?");
+        queryList.add("Is the hotel staff helpful?");
+
+        // Measure execution time for all models
+        measureExecutionTime(models, queryList);
+
     }
 
-    public static void measureExecutionTime(ArrayList<Model> models, String question) throws MalformedURLException, IOException, RepositoryException, MalformedQueryException, QueryEvaluationException {
+    public static void measureExecutionTime(ArrayList<Model> models, ArrayList<String> queryList) throws MalformedURLException, IOException, RepositoryException, MalformedQueryException, QueryEvaluationException {
         //Intance of class timer, for time measurements
         Timer timer = new Timer();
 
-        wordnet_dict.open();
-
         for (Model model : models) {
-            timer.start();
-            model.scoreComments(question);
-            timer.end();
-            long calculateAllScores = timer.getTotalTime();
-            System.out.println("Execution time for model: " + model.getDescription());
-            System.out.println("Total Time to calculate score (for all reviews): " + calculateAllScores);
-            System.out.println("Average Time to calculate score (for one review): " + calculateAllScores / all_comments.size());
+
+            ArrayList<Long> timesPerQuery = new ArrayList<>();
+
+            for (String query : queryList) {
+                timer.start();
+                model.scoreComments(query);
+                timer.end();
+                timesPerQuery.add(timer.getTotalTime());
+            }
+
+            Long totalTime = 0L;
+            for (Long time : timesPerQuery) {
+                totalTime += time;
+            }
+
+            //long calculateAllScores = timer.getTotalTime();
+            System.out.println("Model: " + model.getDescription());
+            System.out.println("Total Time to calculate score for all reviews over all queries: " + totalTime);
+            System.out.println("Average Time to calculate score for all reviews over one query: " + totalTime / queryList.size());
+            System.out.println("Average Time to calculate score for one review over one query: " + (totalTime / queryList.size()) / model.getComments().size());
         }
 
     }
